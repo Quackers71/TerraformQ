50424f3964aab7bd5466acdd378e46c9c2669d69
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-04 20:10:13 +0100

    work in progress - dns Route53 addition

--- moduleone.tf
+++ moduleone.tf
@@ -4,9 +4,9 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "private_key_path" {}
-variable "key_name" {
-  default = "QEC2DPC"
+
+variable "my_region" {
+  default = "eu-west-2"
 }
 
 ##################################################################################
@@ -16,35 +16,25 @@ variable "key_name" {
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "eu-west-2"
 }
 
 ##################################################################################
 # RESOURCES
 ##################################################################################
 
-resource "aws_instance" "nginx" {
-  ami           = "ami-b2b55cd5"
-  instance_type = "t2.micro"
-  key_name        = "${var.key_name}"
-
-  connection {
-    user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start"
-    ]
-  }
+#AWS DNS #
+resource "aws_route53_record" "elb" {
+  name                = "dev-website"
+  zone_id             = "quackers.ninja"
+  type				  = "A"
+  ttl                 = "30"
+  provider            = "aws"
 }
 
 ##################################################################################
 # OUTPUT
 ##################################################################################
 
-output "aws_instance_public_dns" {
-    value = "${aws_instance.nginx.public_dns}"
+output "aws_dns_name" {
+  value = "${aws_route53_record}"
 }
